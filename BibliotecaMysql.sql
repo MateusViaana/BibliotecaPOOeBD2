e07e106f559f39c1781ed9feea3e52fc998b870c
Mateus Viana <mateusviana1400@gmail.com>
2025-02-21 17:03:29 -0400

    testando

--- BibliotecaMysql.sql
+++ BibliotecaMysql.sql
@@ -3,4 +3,5 @@ use biblioteca;
 create table livros (idLivro int primary key auto_increment, titulo varchar(200), autor varchar(200),
 editora varchar (200), anoPublicacao date, isbn char(13), genero varchar(100), edicao varchar(60), idioma varchar(60));
 
-desc livros; 
+desc livros;
+select * from livros; 
---
+++ interface/IDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaPOOeBD2.internal interface
+{
+    public interface IDAO<T>
+    {
+        void Insert (T item)
+        {
+
+        }
+    }
+}
